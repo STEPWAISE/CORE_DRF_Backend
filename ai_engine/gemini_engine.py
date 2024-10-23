16ab4ac902773b601c2c0fe1aa8adb3b06a6669e
arjun <arjunshukla942@gmail.com>
2024-10-23 23:38:58 +0530

    Gemini Engine Base

--- ai_engine/gemini_engine.py
+++ ai_engine/gemini_engine.py
@@ -1,1 +1,24 @@
 # ENGINE
+
+from dotenv import load_dotenv
+import os
+import google.generativeai as ggi
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+# Fetch the API key from the environment variable
+fetched_api_key = os.getenv("GEMINI_KEY")
+
+# Configure the generative AI with the fetched API key
+ggi.configure(api_key=fetched_api_key)
+
+def initialize_model():
+    model = ggi.GenerativeModel("gemini-pro")
+    chat = model.start_chat()
+    return chat
+
+gpt = initialize_model()
+
+response = gpt.send_message("Hi How are you?", stream=True)
+print(response)
